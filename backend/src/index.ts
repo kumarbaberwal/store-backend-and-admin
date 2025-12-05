4ae77058e422da312fff52baff7438ff80d30ffd
kumarbaberwal <itsnitesh16@gmail.com>
2025-12-06 01:50:24 +0530

    start server added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -29,7 +29,11 @@ if (ENV.NODE_ENV === "production") {
 }
 
 
-app.listen(PORT, () => {
-  connectDB();
-  console.log(`Server is running on http://localhost:${PORT}`);
-})
+const startServer = async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  })
+}
+
+startServer();
