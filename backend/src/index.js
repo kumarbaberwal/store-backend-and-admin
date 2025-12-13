c2fc0ab1e68f3e13703c9dda9583ac9a13760cb2
kumarbaberwal <itsnitesh16@gmail.com>
2025-12-13 19:07:45 +0530

    index.js updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,7 +30,7 @@ if (ENV.NODE_ENV === "production") {
 
   app.use(express.static(adminDist));
 
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(adminDist, "index.html"));
   });
 }
