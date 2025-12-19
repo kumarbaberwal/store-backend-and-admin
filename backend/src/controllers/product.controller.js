e6cf915705f2bc60a930c2be2034f2bd3c07b4ae
kumarbaberwal <itsnitesh16@gmail.com>
2025-12-20 00:06:33 +0530

    review, product routes and controllers added

---
+++ backend/src/controllers/product.controller.js
@@ -0,0 +1,24 @@
+import { Product } from "../models/product.model";
+
+export const getProductById = async (req, res) => {
+  try {
+
+    const { id } = req.params;
+
+    const product = await Product.findById(id)
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found"
+      })
+    }
+
+    res.status(200).json(product)
+
+  } catch (error) {
+    console.error("Error in getProductById controller: ", error);
+    res.status(500).json({
+      error: "Internal Server Error",
+    })
+  }
+}
